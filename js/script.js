22ce307f180470a732b6227a179b705fbe2d2ac5
Fares-Abdelkader <124618781+Fares-Abdelkader@users.noreply.github.com>
2023-05-26 17:01:19 +0000

    Index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,43 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+/* Created by: Fares.Abdelkader
+Created on: April 2022
+This file contains the JS functions for index.html*/
+
+'use strict'
+
+/**
+ * Check service worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit6-02-HTML/sw.js", {
+    scope: "/ICS2O-Unit6-02-HTML/",
+  })
+}
+
+/**
+ * Input
+ */
+
+function updateCount() {
+  if (localStorage.clicks) {
+    document.getElementById("output").innerHTML = "Cookies: " + localStorage.clicks
+  } else {
+    localStorage.clicks = 0
+  }
+}
+/**
+ * Resets the click counter
+ */
+function resetCookies() {
+  localStorage.clicks = 0
+  updateCount()
+  console.log(localStorage.clicks)
+}
+
+/**
+ * Adds 1 to the counter
+ */
+function getCookies() {
+  localStorage.clicks++
+  updateCount()
+  console.log(localStorage.clicks)
+}
